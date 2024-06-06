2ca57a196ad795a88a54c83751d462e44b6310cf
suxin123 <2418213125@qq.com>
2024-06-06 15:43:32 +0800

    109-跨链转账详情页面

--- src/app/transfer_function/components/Token.tsx
+++ src/app/transfer_function/components/Token.tsx
@@ -1,6 +1,6 @@
-import DropArrow from "@/components/Icons/DropArrow";
-import EthSVG from "@/components/Icons/EthSVG";
-import { IToken, ITokenBalance, useChains } from "@/store/useChains";
+import DropArrow from '@/components/Icons/DropArrow'
+import EthSVG from '@/components/Icons/EthSVG'
+import { IToken, ITokenBalance, useChains } from '@/store/useChains'
 import {
   Avatar,
   Button,
@@ -11,19 +11,19 @@ import {
   ModalBody,
   ModalContent,
   ModalHeader,
-  useDisclosure,
-} from "@nextui-org/react";
-import { FC, useContext, useEffect, useMemo, useState } from "react";
-import Image from "next/image";
-import { classNames } from "@/utils/classNames";
-import { useRouter } from "next/navigation";
-import { formatValue } from "@/utils/format";
-import { useAddress } from "@/store/useAddress";
-import { chatApi, chatInitApi, vertifyWalletBalanceApi } from "@/api/demand";
-import { BalanceInfo, ChainBalances, CtxBalanceReq } from "@/api/types/demand";
-import { LoadingContext } from "@/app/providers";
-import { EMessage, IResult } from "@/app/demandchat/tyeps";
-export function TokenItem({
+  useDisclosure
+} from '@nextui-org/react'
+import { FC, useContext, useEffect, useMemo, useState } from 'react'
+import Image from 'next/image'
+import { classNames } from '@/utils/classNames'
+import { useRouter } from 'next/navigation'
+import { formatValue } from '@/utils/format'
+import { useAddress } from '@/store/useAddress'
+import { chatApi, chatInitApi, vertifyWalletBalanceApi } from '@/api/demand'
+import { BalanceInfo, ChainBalances, CtxBalanceReq } from '@/api/types/demand'
+import { LoadingContext } from '@/app/providers'
+import { EMessage, IResult } from '@/app/demandchat/tyeps'
+export function TokenItem ({
   tokenAvatarUrl,
   tokenName,
   tokenLabel,
@@ -31,315 +31,315 @@ export function TokenItem({
   price,
   isChosen,
   pickToken,
-  clickEvent,
+  clickEvent
 }: {
-  tokenAvatarUrl: string;
-  tokenName: string;
-  tokenLabel: string;
-  amount: string;
-  price: string;
-  isChosen: boolean;
-  pickToken: (token) => void;
-  clickEvent: () => void;
+  tokenAvatarUrl: string
+  tokenName: string
+  tokenLabel: string
+  amount: string
+  price: string
+  isChosen: boolean
+  pickToken: (token) => void
+  clickEvent: () => void
 }) {
   return (
     <div
-      className="flex justify-between items-center hover:bg-slate-500/30 cursor-pointer p-2 rounded-lg"
-      style={{ color: isChosen ? "#4FAAEB" : "white" }}
+      className='flex justify-between items-center hover:bg-slate-500/30 cursor-pointer p-2 rounded-lg'
+      style={{ color: isChosen ? '#4FAAEB' : 'white' }}
       onClick={() => {
-        pickToken(tokenName);
-        clickEvent();
+        pickToken(tokenName)
+        clickEvent()
       }}
     >
-      <div className="flex gap-2 items-center">
-        <Avatar radius="sm" src={tokenAvatarUrl}></Avatar>
-        <div className="">
+      <div className='flex gap-2 items-center'>
+        <Avatar radius='sm' src={tokenAvatarUrl}></Avatar>
+        <div className=''>
           <p>{tokenName}</p>
           <p>{tokenLabel}</p>
         </div>
       </div>
 
-      <div className=" opacity-0">gap</div>
+      <div className=' opacity-0'>gap</div>
       <div>
         <p>{amount}</p>
         <p>${price}</p>
       </div>
     </div>
-  );
+  )
 }
 
 interface ITokenProps {
-  tokens?: IToken[];
-  balances?: ITokenBalance[];
-  toAddress?: string;
+  tokens?: IToken[]
+  balances?: ITokenBalance[]
+  toAddress?: string
 }
 
-const CIDNAME = "X-Smartwallet-Cid";
+const CIDNAME = 'X-Smartwallet-Cid'
 
-const Token: FC<ITokenProps> = (props) => {
-  const { tokens = [], balances = [], toAddress = "" } = props;
-  const { currentAddress } = useAddress((state) => state);
-  const [assetBalance, setAssetBalance] = useState<ChainBalances>({}); // 资产余额
-  const { currentChain,chains, balances:balancesOfChains } = useChains((state) => state);
-  const router = useRouter();
+const Token: FC<ITokenProps> = props => {
+  const { tokens = [], balances = [], toAddress = '' } = props
+  const { currentAddress } = useAddress(state => state)
+  const [assetBalance, setAssetBalance] = useState<ChainBalances>({}) // 资产余额
+  const {
+    currentChain,
+    chains,
+    balances: balancesOfChains
+  } = useChains(state => state)
+  const router = useRouter()
   const calcTokens = useMemo(() => {
-    const arr = [];
+    const arr = []
     for (let i = 0; i < tokens.length; i++) {
-      const token = tokens[i];
-      const balance = balances.find((b) => b.tokenId === token.tokenId);
+      const token = tokens[i]
+      const balance = balances.find(b => b.tokenId === token.tokenId)
       arr.push({
         ...token,
-        amount: balance?.amount || "0",
-        usdValue: balance?.usdValue || "0",
-      });
+        amount: balance?.amount || '0',
+        usdValue: balance?.usdValue || '0'
+      })
     }
-    return arr;
-  }, [tokens, balances]);
+    return arr
+  }, [tokens, balances])
   const [chatHeader, setChatHeader] = useState<{ [CIDNAME]: string }>({
-    [CIDNAME]: "",
-  });
+    [CIDNAME]: ''
+  })
   const [currentToken, setCurrentToken] = useState<
     IToken & { amount: string; usdValue: string }
-  >();
-  const [amount, setAmount] = useState("");
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const { setLoading } = useContext(LoadingContext);
+  >()
+  const [amount, setAmount] = useState('')
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const { setLoading } = useContext(LoadingContext)
 
   const usdBan = currentToken
     ? Number(currentToken?.amount) != 0
       ? (+currentToken?.usdValue / +currentToken?.amount) * +amount
       : 0
-    : 0;
-      const cacheChainMap = new Map();
+    : 0
+  const cacheChainMap = new Map()
   const getTokenName = (chainName: string, tokenId: number) => {
-    const cacheTokenName = cacheChainMap.get(`${chainName}-${tokenId}`);
-    if (cacheTokenName) return cacheTokenName;
+    const cacheTokenName = cacheChainMap.get(`${chainName}-${tokenId}`)
+    if (cacheTokenName) return cacheTokenName
     // 做个缓存
-    const targetChain = chains.find((chain) => chain.name == chainName);
-    if (!targetChain) return "";
+    const targetChain = chains.find(chain => chain.name == chainName)
+    if (!targetChain) return ''
     const targetToken = targetChain.tokens.find(
-      (token) => token.tokenId == tokenId
-    );
-    if (!targetToken) return "";
-    cacheChainMap.set(`${chainName}-${tokenId}`, targetToken.name);
-    return targetToken.name;
-  };
- const convert2balances = () => {
-    const _balances: ChainBalances = {};
+      token => token.tokenId == tokenId
+    )
+    if (!targetToken) return ''
+    cacheChainMap.set(`${chainName}-${tokenId}`, targetToken.name)
+    return targetToken.name
+  }
+  const convert2balances = () => {
+    const _balances: ChainBalances = {}
     // 找当前链的
-    balancesOfChains.forEach((chain) => {
-      const chainName = chain.chainName;
-      const _tokenList: BalanceInfo[] = [];
-      chain.tokenBalance.forEach((token) => {
+    balancesOfChains.forEach(chain => {
+      const chainName = chain.chainName
+      const _tokenList: BalanceInfo[] = []
+      chain.tokenBalance.forEach(token => {
         // 获取每个chain的balance
         // 根据 chainName 和 tokenid 找到 tokenname
-        const tokenName = getTokenName(chainName, token.tokenId);
+        const tokenName = getTokenName(chainName, token.tokenId)
         _tokenList.push({
           balance: Number(token.amount),
-          symbol: tokenName,
-        });
-        _balances[chainName] = _tokenList;
+          symbol: tokenName
+        })
+        _balances[chainName] = _tokenList
         // 将balance转换成 ChainBalance 类型
-      });
-    });
-    setAssetBalance(_balances);
-    console.log(_balances, "_balances");
-    return _balances;
-  };
+      })
+    })
+    setAssetBalance(_balances)
+    console.log(_balances, '_balances')
+    return _balances
+  }
   // 页面默认选择
   useEffect(() => {
-    const find = calcTokens.find((item) => Number(item?.amount) > 0);
+    const find = calcTokens.find(item => Number(item?.amount) > 0)
     if (find) {
-      setCurrentToken(find);
+      setCurrentToken(find)
     }
-  }, [calcTokens]);
+  }, [calcTokens])
 
   useEffect(() => {
-    initChatCid();
-      setTimeout(() => {
-      convert2balances();
-    }, 100);
-  }, []);
+    initChatCid()
+    setTimeout(() => {
+      convert2balances()
+    }, 100)
+  }, [])
   const initChatCid = async () => {
-    const { status, body } = await chatInitApi();
-    debugger;
-    console.log(body!.cid, "body!.cid");
+    const { status, body } = await chatInitApi()
+    // debugger;
+    console.log(body!.cid, 'body!.cid')
     setChatHeader({
-      [CIDNAME]: body!.cid,
-    });
-  };
+      [CIDNAME]: body!.cid
+    })
+  }
   // 获取evm地址
   const vertifyWalletBalance = async () => {
-    if (!currentAddress) return false;
+    if (!currentAddress) return false
     const param: CtxBalanceReq = {
-      address: currentAddress || "",
+      address: currentAddress || '',
       // 目前固定写死 mumbai
       baseChain: currentChain?.name as string,
-      balances: assetBalance,
-    };
-    const { status, body } = await vertifyWalletBalanceApi(param, chatHeader);
-    if (status && status === 200) {
-      return true;
-    } else {
-      return false;
+      balances: assetBalance
     }
-  };
-  function handleTransferAll() {
+    const { status, body } = await vertifyWalletBalanceApi(param, chatHeader)
+    if (status && status === 200) {
+      return true
+    } else {
+      return false
+    }
+  }
+  function handleTransferAll () {
     if (currentToken) {
-      setAmount(currentToken.amount);
+      setAmount(currentToken.amount)
     }
   }
   const handleNext = async () => {
     const res = await handleRequest(
       `I want to transfer ${amount} ${
         currentToken?.name
-      } to ${toAddress} on ${
-        currentChain?.name.toLowerCase()
-      }`
-    // `I want to transfer 0.005 SWT to 0x539a44bfd3915718fc450c34366d2fbfef13672e on sepolia`
-
-    );
+      } to ${toAddress} on ${currentChain?.name.toLowerCase()}`
+      // `I want to transfer 0.005 SWT to 0x539a44bfd3915718fc450c34366d2fbfef13672e on sepolia`
+    )
     if (currentToken && currentChain && res.response.ops) {
-      sessionStorage.setItem("transfer_tokenId", currentToken.tokenId + "");
-      sessionStorage.setItem("transfer_amount", amount);
-      sessionStorage.setItem("transfer_tokenName", currentToken.name);
-      if (res.response.ops.length>1) {
-        debugger;
-      sessionStorage.setItem("transfer_Info", JSON.stringify(res.response));
+      sessionStorage.setItem('transfer_tokenId', currentToken.tokenId + '')
+      sessionStorage.setItem('transfer_amount', amount)
+      sessionStorage.setItem('transfer_tokenName', currentToken.name)
+      if (res.response.ops.length > 1) {
+        sessionStorage.setItem('transfer_Info', JSON.stringify(res.response))
       }
 
-      router.push("/confirmation");
+      router.push('/confirmation')
     }
-  };
+  }
   const handleRequest = async (inputDemandText: string): Promise<any> => {
     return new Promise(async (resolve, reject) => {
       try {
-        setLoading(true);
-        const res = await vertifyWalletBalance();
+        setLoading(true)
+        const res = await vertifyWalletBalance()
         if (!res) {
-          setLoading(false);
-          return reject("Wallet balance verification failed");
+          setLoading(false)
+          return reject('Wallet balance verification failed')
         }
 
-        const { body } = await chatApi(inputDemandText, chatHeader);
-        setLoading(false);
-        const { category, detail } = body as IResult;
-        const { ops, reply } = detail;
+        const { body } = await chatApi(inputDemandText, chatHeader)
+        setLoading(false)
+        const { category, detail } = body as IResult
+        const { ops, reply } = detail
 
-        console.log(ops, "ops");
+        console.log(ops, 'ops')
 
         if (!ops || !ops.length) {
           const newMsg = {
             content: reply,
             msgType: EMessage.MSG,
-            response: detail,
-          };
-          return resolve(newMsg);
+            response: detail
+          }
+          return resolve(newMsg)
         } else {
-          const targetOp = ops[0];
-          let msgType = category || targetOp.type;
+          const targetOp = ops[0]
+          let msgType = category || targetOp.type
           switch (category) {
             case EMessage.SWAP:
-              msgType = EMessage.SWAP;
-              break;
+              msgType = EMessage.SWAP
+              break
             case EMessage.TRANSFER:
-              msgType = EMessage.TRANSFER;
-              break;
+              msgType = EMessage.TRANSFER
+              break
             case EMessage.CROSSCHAIN:
-              msgType = EMessage.CROSSCHAIN;
-              break;
+              msgType = EMessage.CROSSCHAIN
+              break
             default:
               // 处理未知类型
-              break;
+              break
           }
 
           const newMsg = {
             content: reply,
             msgType: msgType || EMessage.MSG,
-            response: detail,
-          };
-          return resolve(newMsg);
+            response: detail
+          }
+          return resolve(newMsg)
         }
       } catch (error) {
-        setLoading(false);
-        return reject(error);
+        setLoading(false)
+        return reject(error)
       }
-    });
-  };
+    })
+  }
 
   return (
     <>
-      <div className="my-4 font-bold">Token</div>
+      <div className='my-4 font-bold'>Token</div>
       <Input
-        variant="bordered"
+        variant='bordered'
         readOnly
-        value={currentToken?.name || ""}
+        value={currentToken?.name || ''}
         onClick={() => onOpen()}
         startContent={
           currentToken ? (
             <Image
               src={currentToken?.icon}
-              alt="logo"
+              alt='logo'
               width={36}
               height={36}
-              className="mr-4"
+              className='mr-4'
             />
           ) : null
         }
         endContent={<DropArrow />}
-        className="mb-4"
+        className='mb-4'
       />
       <Modal
         isOpen={isOpen}
-        placement="bottom"
-        className="text-white"
+        placement='bottom'
+        className='text-white'
         onOpenChange={onOpenChange}
       >
         <ModalContent>
-          {(onClose) => (
+          {onClose => (
             <>
-              <ModalHeader className="flex justify-center items-center text-base">
+              <ModalHeader className='flex justify-center items-center text-base'>
                 Token
               </ModalHeader>
-              <ModalBody className="px-4 pb-4">
+              <ModalBody className='px-4 pb-4'>
                 <Listbox
                   items={calcTokens}
-                  aria-label="Dynamic Actions"
-                  onAction={(key) => {
-                    const find = calcTokens.find((item) => item.tokenId == key);
+                  aria-label='Dynamic Actions'
+                  onAction={key => {
+                    const find = calcTokens.find(item => item.tokenId == key)
                     if (parseFloat(find?.amount!) == 0) {
-                      return;
+                      return
                     }
-                    setCurrentToken(find!);
-                    onClose();
+                    setCurrentToken(find!)
+                    onClose()
                   }}
                 >
-                  {(item) => (
+                  {item => (
                     <ListboxItem
                       // 不可点击
                       isDisabled={parseFloat(item.amount) == 0}
                       key={item.tokenId}
                       textValue={item.name}
                       className={classNames(
-                        "mb-4",
+                        'mb-4',
                         currentToken?.tokenId === item.tokenId
-                          ? "text-[#4FAAEB]"
-                          : ""
+                          ? 'text-[#4FAAEB]'
+                          : ''
                       )}
                     >
-                      <div className="flex items-center">
+                      <div className='flex items-center'>
                         <Image
                           src={item.icon}
-                          alt="logo"
+                          alt='logo'
                           width={40}
                           height={40}
-                          className="mr-4"
+                          className='mr-4'
                         />
-                        <div className="flex-1 font-bold text-base">
+                        <div className='flex-1 font-bold text-base'>
                           {item.name}
                         </div>
-                        <div className="text-right">
-                          <p className="font-bold">
+                        <div className='text-right'>
+                          <p className='font-bold'>
                             {formatValue(item.amount)}
                           </p>
                           <p>${formatValue(item.usdValue)}</p>
@@ -354,45 +354,45 @@ const Token: FC<ITokenProps> = (props) => {
         </ModalContent>
       </Modal>
       <Input
-        label="Amount"
-        variant="bordered"
+        label='Amount'
+        variant='bordered'
         value={amount}
-        onChange={(e) => {
-          setAmount(e.target.value);
+        onChange={e => {
+          setAmount(e.target.value)
         }}
-        className="mb-4"
+        className='mb-4'
       />
 
-      <div className="flex items-center mb-4">
-        <div className="flex-1 flex">
-          <p className="text-sm">{formatValue(usdBan + "")} USD</p>
-          <p className="text-sm mx-4">
-            <span className="font-bold">Balance:</span>
-            <span>{formatValue(currentToken?.amount || "0")}</span>
+      <div className='flex items-center mb-4'>
+        <div className='flex-1 flex'>
+          <p className='text-sm'>{formatValue(usdBan + '')} USD</p>
+          <p className='text-sm mx-4'>
+            <span className='font-bold'>Balance:</span>
+            <span>{formatValue(currentToken?.amount || '0')}</span>
             <span>{currentToken?.name}</span>
           </p>
         </div>
         {currentToken ? (
           <Button
-            size="sm"
-            className=" bg-mainPurpleBlue/30 text-mainPurpleBlue"
+            size='sm'
+            className=' bg-mainPurpleBlue/30 text-mainPurpleBlue'
             onClick={handleTransferAll}
           >
             Transfer all
           </Button>
         ) : null}
       </div>
-      <div className="absolute bottom-8 left-8 right-8">
+      <div className='absolute bottom-8 left-8 right-8'>
         <Button
           onClick={handleNext}
           fullWidth
-          size="lg"
-          className="bg-[#819DF5] rounded-3xl"
+          size='lg'
+          className='bg-[#819DF5] rounded-3xl'
         >
           Next
         </Button>
       </div>
     </>
-  );
-};
-export default Token;
+  )
+}
+export default Token
--- src/app/transfer_function/page.tsx
+++ src/app/transfer_function/page.tsx
@@ -1,66 +1,63 @@
-"use client";
-import Sent from "./components/Sent";
-import Contact, { ConfirmFunc, ContactData } from "./components/Contact";
-import { Button } from "@nextui-org/react";
-import { useState } from "react";
-import { IChain } from "@/store/useChains";
-import { useRouter } from "next/navigation";
-import Toast from "@/utils/toast";
+'use client'
+import Sent from './components/Sent'
+import Contact, { ConfirmFunc, ContactData } from './components/Contact'
+import { Button } from '@nextui-org/react'
+import { useState } from 'react'
+import { IChain } from '@/store/useChains'
+import { useRouter } from 'next/navigation'
+import Toast from '@/utils/toast'
 
-export default function Transfer_Function() {
-  const [address, setAddress] = useState("");
-  const [chain, setChain] = useState<IChain>();
-  const router = useRouter();
+export default function Transfer_Function () {
+  const [address, setAddress] = useState('')
+  const [chain, setChain] = useState<IChain>()
+  const router = useRouter()
 
-  function handleNext() {
+  function handleNext () {
     if (!/^(0x)?[0-9a-fA-F]{40}$/.test(address)) {
-      Toast("The address format is incorrect")
+      Toast('The address format is incorrect')
       return
     }
-    debugger;
+    // debugger;
     if (address && chain) {
-      const _cl = localStorage.getItem("contact_list")
-      const contactList = _cl ? JSON.parse(_cl) : [];
+      const _cl = localStorage.getItem('contact_list')
+      const contactList = _cl ? JSON.parse(_cl) : []
       const contact = contactList.find(
-        (item: ContactData) => item.address == address && item.chain.id == chain.ID.toString()
+        (item: ContactData) =>
+          item.address == address && item.chain.id == chain.ID.toString()
       )
-      sessionStorage.setItem("transfer_name", contact?.name || "")
-      sessionStorage.setItem("transfer_address", address);
-      sessionStorage.setItem("transfer_chainId", chain.ID.toString());
-      sessionStorage.setItem("transfer_chainName", chain.name);
-      router.push("/chooseToken");
+      sessionStorage.setItem('transfer_name', contact?.name || '')
+      sessionStorage.setItem('transfer_address', address)
+      sessionStorage.setItem('transfer_chainId', chain.ID.toString())
+      sessionStorage.setItem('transfer_chainName', chain.name)
+      router.push('/chooseToken')
     }
   }
-  const confirmContact: ConfirmFunc = ({
-    addr,
-    chain_id,
-    name
-  }) => {
-    sessionStorage.setItem("transfer_address", addr);
-    sessionStorage.setItem("transfer_chainId", chain_id.toString());
-    sessionStorage.setItem("transfer_name", name);
-    router.push("/chooseToken");
+  const confirmContact: ConfirmFunc = ({ addr, chain_id, name }) => {
+    sessionStorage.setItem('transfer_address', addr)
+    sessionStorage.setItem('transfer_chainId', chain_id.toString())
+    sessionStorage.setItem('transfer_name', name)
+    router.push('/chooseToken')
   }
   return (
-    <div className="w-full">
+    <div className='w-full'>
       <Sent
         address={address}
         chain={chain}
         setAddress={setAddress}
         setChain={setChain}
       />
-      <div className="h-6"></div>
+      <div className='h-6'></div>
       <Contact confirmContact={confirmContact} />
-      <div className="absolute bottom-8 left-8 right-8">
+      <div className='absolute bottom-8 left-8 right-8'>
         <Button
           onClick={handleNext}
           fullWidth
-          size="lg"
-          className="bg-[#819DF5] rounded-3xl"
+          size='lg'
+          className='bg-[#819DF5] rounded-3xl'
         >
           Next
         </Button>
       </div>
     </div>
-  );
+  )
 }
